c943d4c6955579f453d61aff88be5b2e5ace3035
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-05-29 10:24:57 +0500

    Изменения в методах входа/регистрации

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -14,28 +14,36 @@ async function verifyPassword(password, storedHash) {
     return await bcrypt.compare(password, storedHash);
 }
 
-router.get('/check', (req, res) => {
+router.get('/check', async (req, res) => {
     try {
         const token = req.cookies.token;
 
         if (!token) {
-            return res.status(401).json({ isAuthenticated: false });
+            return res.status(401).json({ isAuthenticated: false, message: 'The token does not exist' });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        return req.status(201).json({
+        const { data, error } = await supabase
+            .from('users')
+            .select('*')
+            .eq('id', decoded.userId)
+            .limit(1)
+        
+        const user = data[0];
+
+        return res.status(201).json({
             isAuthenticated: true,
             user: {
-                id: decoded.userId
-                ,email: decoded.email
-                ,first_name: decoded.first_name
-                ,last_name: decoded.last_name
-                ,middle_name: decoded.middle_name
+                id: user.userId
+                ,email: user.email
+                ,first_name: user.first_name
+                ,last_name: user.last_name
+                ,middle_name: user.middle_name
             }
         });
     } catch (err) {
-        res.status(401).json({ isAuthenticated: false });
+        res.status(500).json({ isAuthenticated: false, message: `${err}` });
     }
 });
 
@@ -45,18 +53,19 @@ router.post('/signup', async (req, res) => {
 
         if (!email || !password || !first_name || !last_name) {
             return res.status(400).json({
-                error: 'Email, password, forst name and last name are required'
+                success: false,
+                message: 'Email, password, forst name and last name are required'
             });
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-            return res.status(400).json({ error: 'Invalid email format' });
-        }
+        if (!emailRegex.test(email))
+            return res.status(400).json({ success: false, message: 'Invalid email format' });
 
         if (password.length < 8) {
             return res.status(400).json({
-                error: 'Password must be at least 8 characters long'
+                success: false,
+                message: 'Password must be at least 8 characters long'
             });
         }
 
@@ -68,12 +77,11 @@ router.post('/signup', async (req, res) => {
         
         if (lookupError) {
             console.error('Supabase lookup error:', lookupError);
-            return res.status(500).json({ error: 'Dtaabase error' });
+            return res.status(500).json({ success: false, message: 'Dtaabase error' });
         }
 
-        if (existingUsers.length > 0) {
-            return res.status(409).json({ error: 'Email already exists' });
-        }
+        if (existingUsers.length > 0)
+            return res.status(409).json({ success: false, message: 'Email already exists' });
 
         const hashedPassword = hashPassword(password);
 
@@ -90,20 +98,11 @@ router.post('/signup', async (req, res) => {
         
         if (createError) {
             console.error('Supabase error:', createError);
-            return res.status(500).json({ error: 'Failed to create user' });
+            return res.status(500).json({ success: false, message: 'Failed to create user' });
         }
 
-        const token = jwt.sign(
-            {
-                userId: newUser[0].id
-                ,email: newUser[0].email
-                ,first_name: newUser.first_name
-                ,last_name: newUser.last_name
-                ,middle_name: newUser.middle_name
-            },
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' }
-        );
+        // Генерация jwt-токена
+        const token = jwt.sign({ userId: user[0].id },process.env.JWT_SECRET, { expiresIn: '24h' });
 
         res.cookie('token', token, {
             httpOnly: true,
@@ -114,26 +113,28 @@ router.post('/signup', async (req, res) => {
 
         return res.status(200).json({
             message: 'User created successfully',
+            success: true,
             user: {
-                id: newUser[0].id,
-                email: newUser[0].email,
-                first_name: newUser[0].first_name,
-                last_name: newUser[0].last_name
-            },
-            token
+                id: newUser[0].id
+                ,email: newUser[0].email
+                ,first_name: newUser[0].first_name
+                ,last_name: newUser[0].last_name
+                ,middle_name: newUser[0].middle_nam
+            }
         });
 
     } catch (err) {
         console.log('Error:', err);
+        return res.status(401).json({ success: false });
     }
 });
 
-router.post('/login', validateAuthToken, async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return res.status(400).json({ error: 'Enail and passwordare required' });
+            return res.status(400).json({ success: false, message: 'Enail and passwordare required' });
         }
 
         const { data: users, error } = await supabase
@@ -144,11 +145,11 @@ router.post('/login', validateAuthToken, async (req, res) => {
 
         if (error) {
             console.error('Supabase error:', err);
-            return res.status(500).json({ error: 'Database error' });
+            return res.status(500).json({ success: false, message: 'Database error' });
         }
 
         if (users.length === 0) {
-            return res.status(401).json({ error: 'Invalid email or password' });
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
         }
 
         const user = users[0];
@@ -156,20 +157,11 @@ router.post('/login', validateAuthToken, async (req, res) => {
         const isPasswordValid = verifyPassword(password, user.password);
 
         if (!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid email or password' });
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
         }
 
-        const token = jwt.sign(
-            {
-                userId: user[0].id
-                ,email: user[0].email
-                ,first_name: user.first_name
-                ,last_name: user.last_name
-                ,middle_name: user.middle_name
-            },
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' }
-        );
+        // Генерация jwt-токена
+        const token = jwt.sign({ userId: user[0].id },process.env.JWT_SECRET, { expiresIn: '24h' });
 
         res.cookie('token', token, {
             httpOnly: true,
@@ -180,7 +172,14 @@ router.post('/login', validateAuthToken, async (req, res) => {
 
         return res.status(200).json({
             message: 'Login successful',
-            user: { id: user.id, email: user.email }
+            success: true,
+            user: {
+                id: user.id
+                ,email: user.email
+                ,first_name: user.first_name
+                ,last_name: user.last_name
+                ,middle_name: user.middle_nam
+            }
         });
     } catch (err) {
         console.error('Error:', err);
